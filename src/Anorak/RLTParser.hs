4dc45d110fd94aa2e2d15fad772dd6607362feac
dwdyer <dan@uncommons.org>
2008-11-20 16:25:14 +0000

    Eliminated separate Adjustment type.

--- src/Anorak/RLTParser.hs
+++ src/Anorak/RLTParser.hs
@@ -11,10 +11,10 @@ import System.Locale(defaultTimeLocale)
 import Text.ParserCombinators.Parsec
 
 -- | An RLT file consists of many items (results, metadata and comments).
-data Item = Fixture Result    -- ^ The result of a single football match.
-          | Points Adjustment -- ^ A number of points awarded to or deducted from an individual team.
-          | Metadata [String] -- ^ Data about the league, such as which positions are promoted or relegated.
-          | Comment String    -- ^ Comments about the data.
+data Item = Fixture Result        -- ^ The result of a single football match.
+          | Adjustment Team Int   -- ^ A number of points awarded to or deducted from an individual team.
+          | Metadata [String]     -- ^ Data about the league, such as which positions are promoted or relegated.
+          | Comment String        -- ^ Comments about the data.
 
 -- | Parse results, discard meta-data and comments.
 results :: Parser ([Result], Map Team Int)
@@ -33,8 +33,8 @@ record = do fields <- sepBy field (char '|')
                 (date:hTeam:hGoals:aTeam:aGoals:_) -> return (Fixture (Result day hTeam (read hGoals) aTeam (read aGoals)))
                                                       -- RLT dates are 8-character strings in DDMMYYYY format.
                                                       where day = readTime defaultTimeLocale "%d%m%Y" date
-                ("AWARDED":team:points:[])         -> return (Points (Adjustment team (read points)))
-                ("DEDUCTED":team:points:[])        -> return (Points (Adjustment team (-read points)))
+                ("AWARDED":team:points:[])         -> return (Adjustment team (read points))
+                ("DEDUCTED":team:points:[])        -> return (Adjustment team (-read points))
                 otherwise                          -> return (Metadata fields)
 
 field = many1 (noneOf "|\n")
@@ -48,16 +48,16 @@ comment = do char '#'
 -- | Takes a list of parsed items and discards comments and meta-data.  The remaining items are separated into a list
 --   of results and a map of net points adjustments by team.
 extractData :: [Item] -> ([Result], Map Team Int) -> ([Result], Map Team Int)
-extractData [] (r, a)                        = (sort r, a) -- Sort results as the final operation because otherwise they are backwards.
-extractData (Fixture result:items) (r, a)    = extractData items (result:r, a)
-extractData (Points adjustment:items) (r, a) = extractData items (r, addAdjustment adjustment a)
-extractData (_:items) (r, a)                 = extractData items (r, a)
+extractData [] (r, a)                             = (sort r, a) -- Sort results as the final operation because otherwise they are backwards.
+extractData (Fixture result:items) (r, a)         = extractData items (result:r, a)
+extractData (Adjustment team amount:items) (r, a) = extractData items (r, addAdjustment team amount a)
+extractData (_:items) (r, a)                      = extractData items (r, a)
 
 -- | Adds a points adjustment to a map of existing points adjustments.  All adjustments for an individual team are
 --   collated into a single entry.
-addAdjustment :: Adjustment -> Map Team Int -> Map Team Int
-addAdjustment adjustment map = Map.insert (team adjustment) total map
-                               where total = (amount adjustment) + Map.findWithDefault 0 (team adjustment) map 
+addAdjustment :: Team -> Int -> Map Team Int -> Map Team Int
+addAdjustment team amount  map = Map.insert team total map
+                                 where total = amount + Map.findWithDefault 0 team map 
 
 -- | Parses the given string (the content of an RLT file) and returns a list of football results.
 parseRLT :: String -> Either ParseError ([Result], Map Team Int)
--- src/Anorak/Types.hs
+++ src/Anorak/Types.hs
@@ -1,5 +1,5 @@
 -- | Core data types used by the Anorak system.
-module Anorak.Types (Team, Result(Result), Adjustment(Adjustment, team, amount)) where
+module Anorak.Types (Team, Result(Result)) where
 
 import Data.Time.Calendar(Day)
 import Data.Time.Format(formatTime)
@@ -28,12 +28,4 @@ instance Ord Result where
     compare result1 result2
         | (date result1) == (date result2) = compare (homeTeam result1) (homeTeam result2)
         | otherwise                        = compare (date result1) (date result2)
-
--- | An adjustment adds or subtracts league points from a team.  It is typically used to deduct
---   points from a team that has infringed the competition rules, although it may also be used
---   to add points to a team's total.
-data Adjustment = Adjustment {team :: Team,
-                              amount :: Int}
-instance Show Adjustment where
-    show adjustment = team adjustment ++ " " ++ show (amount adjustment)
 
