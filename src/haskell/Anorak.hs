d7934c539dfe0941533fda2550e832d8f9934397
dwdyer <dan@uncommons.org>
2008-11-24 00:48:22 +0000

    Copy resource files (stylesheet etc.)

--- src/haskell/Anorak.hs
+++ src/haskell/Anorak.hs
@@ -5,22 +5,25 @@ import Anorak.Types
 import Anorak.RLTParser
 import Data.Map(Map)
 import qualified Data.Map as Map
-import List(sort)
+import List(isPrefixOf, isSuffixOf, sort)
+import Monad(filterM)
 import System(getArgs)
+import System.Directory(copyFile, doesFileExist, getDirectoryContents)
+import System.FilePath(combine, replaceDirectory, takeFileName)
 import Text.ParserCombinators.Parsec(ParseError)
 import Text.StringTemplate
 import Text.StringTemplate.Classes
 
 instance ToSElem LeagueRecord where
-    toSElem record = (LI [STR $ team record,
-                          STR $ show $ played record,
-                          STR $ show $ won record,
-                          STR $ show $ drawn record,
-                          STR $ show $ lost record,
-                          STR $ show $ for record,
-                          STR $ show $ against record,
-                          STR $ show $ goalDiff record,
-                          STR $ show $ points record])
+    toSElem record = SM $ Map.fromList [("team", toSElem $ team record),
+                                        ("played", toSElem $ played record),
+                                        ("won", toSElem $ won record),
+                                        ("drawn", toSElem $ drawn record),
+                                        ("lost", toSElem $ lost record),
+                                        ("for", toSElem $ for record),
+                                        ("against", toSElem $ against record),
+                                        ("goalDiff", toSElem $ goalDiff record),
+                                        ("points", toSElem $ points record)]
 
 -- | Builds a LeagueRecord for the specified team, including all of the results (from those provided) in which that
 --   team was involved.
@@ -60,13 +63,33 @@ adjust adjustments (LeagueRecord t w d l f a adj) = (LeagueRecord t w d l f a (a
 htmlLeagueTable :: [LeagueRecord] -> StringTemplate String -> String
 htmlLeagueTable table template = toString $ setAttribute "table" table template
 
-main :: IO()
-main = do dataFile:templateDir:_ <- getArgs
+-- | Copies all non-template files from the source directory to the target directory.  Used for making sure that CSS
+--   files and images (if any) are deployed with the generated HTML.
+copyResources :: FilePath -> FilePath -> IO ()
+copyResources from to = do files <- getDirectoryContents from
+                           let absoluteFiles = map (combine from) files -- Convert file names into absolute paths.
+                           resources <- filterM (isResourceFile) absoluteFiles
+                           print resources
+                           mapM_ (copyToDirectory to) resources
+
+-- | Predicate for filtering.  Accepts files that are not templates, not directories and not hidden files.
+isResourceFile :: FilePath -> IO Bool
+isResourceFile path = do ordinaryFile <- doesFileExist path
+                         return (ordinaryFile
+                                 && (not $ isSuffixOf ".st" path) -- File name does not have the template extension.
+                                 && (not $ isPrefixOf "." $ takeFileName path)) -- File is not a hidden file.
+
+-- | Copies an individual file to a new directory, retaining the original file name.
+copyToDirectory :: FilePath -> FilePath -> IO()
+copyToDirectory dir file = copyFile file (replaceDirectory file dir)
+
+main :: IO ()
+main = do dataFile:templateDir:outputDir:_ <- getArgs
           (teams, results, adjustments) <- parseRLTFile dataFile
           group <- directoryGroup templateDir :: IO (STGroup String)
           case getStringTemplate "leaguetable.html" group of
-              Nothing       -> print "Error"
-              Just template -> print $ htmlLeagueTable table template
+              Nothing       -> print "Could not find league table template."
+              Just template -> writeFile (outputDir ++ "/leaguetable.html") $ htmlLeagueTable table template
                                where table = leagueTable (resultsByTeam results Map.empty) adjustments
-          
+          copyResources templateDir outputDir          
 
