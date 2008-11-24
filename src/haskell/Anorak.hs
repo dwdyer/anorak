83a3e2f5775251e45522d8e8bd822a6cac8d04cf
dwdyer <dan@uncommons.org>
2008-11-24 01:08:07 +0000

    Separated out core functionality into new module.

--- src/haskell/Anorak.hs
+++ src/haskell/Anorak.hs
@@ -1,11 +1,12 @@
 -- | Main module for the Anorak system.
-module Anorak (leagueTable) where
+module Anorak where
 
+import Anorak.Core
 import Anorak.Types
 import Anorak.RLTParser
 import Data.Map(Map)
 import qualified Data.Map as Map
-import List(isPrefixOf, isSuffixOf, sort)
+import List(isPrefixOf, isSuffixOf)
 import Monad(filterM)
 import System(getArgs)
 import System.Directory(copyFile, doesFileExist, getDirectoryContents)
@@ -24,41 +25,6 @@ instance ToSElem LeagueRecord where
                                         ("against", toSElem $ against record),
                                         ("goalDiff", toSElem $ goalDiff record),
                                         ("points", toSElem $ points record)]
-
--- | Builds a LeagueRecord for the specified team, including all of the results (from those provided) in which that
---   team was involved.
-buildRecord :: Team -> [Result] -> LeagueRecord
-buildRecord team results = foldl (addResultToRecord team) (LeagueRecord team 0 0 0 0 0 0) results
-
--- | Adds a single match result to a particular team's league record.  If the specified team was not involved in that
---   match, the match is ignored.
-addResultToRecord :: Team -> LeagueRecord -> Result -> LeagueRecord
-addResultToRecord team record result 
-    | team == (homeTeam result) = addScoreToRecord record (homeGoals result) (awayGoals result)
-    | team == (awayTeam result) = addScoreToRecord record (awayGoals result) (homeGoals result)
-    | otherwise                 = record
-
-addScoreToRecord :: LeagueRecord -> Int -> Int -> LeagueRecord
-addScoreToRecord (LeagueRecord team won drawn lost for against adjustment) scored conceded
-    | scored > conceded  = (LeagueRecord team (won + 1) drawn lost (for + scored) (against + conceded) adjustment)
-    | scored == conceded = (LeagueRecord team won (drawn + 1) lost (for + scored) (against + conceded) adjustment)
-    | otherwise          = (LeagueRecord team won drawn (lost + 1) (for + scored) (against + conceded) adjustment)
-
--- | Convert a flat list of results into a mapping from team to list of results that that team was involved in.
-resultsByTeam :: [Result] -> Map Team [Result] -> Map Team [Result]
-resultsByTeam [] map          = map
-resultsByTeam (result:rs) map = resultsByTeam rs (Map.insertWith (++) (awayTeam result) [result] map')
-                                where map' = Map.insertWith (++) (homeTeam result) [result] map
-
--- | Produces a standard league table with teams ordered in descending order of points.  Takes a map of teams to
---   results and a map of points adjustments and returns a sorted list of league records.
-leagueTable :: Map Team [Result] -> Map Team Int -> [LeagueRecord]
-leagueTable teamResults adjustments  = sort $ map (adjust adjustments) table
-                                       where table = Map.elems $ Map.mapWithKey (buildRecord) teamResults
-
--- | Looks up the points adjustment for a team (if any) and applies it to their league record.
-adjust :: Map Team Int -> LeagueRecord -> LeagueRecord
-adjust adjustments (LeagueRecord t w d l f a adj) = (LeagueRecord t w d l f a (adj + Map.findWithDefault 0 t adjustments))
 
 htmlLeagueTable :: [LeagueRecord] -> StringTemplate String -> String
 htmlLeagueTable table template = toString $ setAttribute "table" table template
