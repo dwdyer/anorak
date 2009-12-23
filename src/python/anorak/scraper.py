b553efa092cf6e6678ca2b3069623c71f75820ca
dwdyer <dan@uncommons.org>
2009-12-23 20:31:50 +0000

    Improvements to Python script.

--- src/python/anorak/scraper.py
+++ src/python/anorak/scraper.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import urllib
-from BeautifulSoup import BeautifulSoup
-from aliases import aliases
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+from config import aliases, data
 
 class Result:
     def __init__(self, date, home_team, home_score, away_team, away_score):
+        """Sets all result fields, mapping team names to their canonical forms."""
         self.date = date
         self.home_team = home_team if not home_team in aliases else aliases[home_team]
         self.home_score = home_score
@@ -12,37 +13,45 @@ class Result:
         self.away_score = away_score
 
     def __str__(self):
+        """Returns the result formatted as a single RLT (pipe-delimited) record."""
         return "|".join([self.date.strftime("%d%m%Y"), self.home_team, str(self.home_score), self.away_team, str(self.away_score)])
 
     def __cmp__(self, other):
+        """Results are ordered primarily by date.  Results from the same day are sorted alphabetically by home team."""
         date_cmp = cmp(self.date, other.date)
         return date_cmp if date_cmp != 0 else cmp(self.home_team, other.home_team)
 
 
-def load_html(url):
+def load_html(url, strainer=None):
     """Loads the HTML at the specified URL and returns it as a BeautifulSoup object."""
     urlReader = urllib.urlopen(url)
     html = urlReader.read()
     urlReader.close()
-    return BeautifulSoup(html)
+    return BeautifulSoup(html, convertEntities=BeautifulSoup.ALL_ENTITIES, parseOnlyThese=strainer)
 
 def scrape_bbc_results(results_page_url):
     """Scrapes the specified BBC web page for football results.  Returns a list of Result objects."""
-    soup = load_html(results_page_url)
+    tags = load_html(results_page_url, SoupStrainer(attrs={"class":["mvb", "competitionResults"]}))
     # Find all tags that contain dates or results ('mvb' indicates a date, 'competitionResults' indicates a result).
-    tags = soup.findAll(attrs={"class":["mvb", "competitionResults"]})
     results = []
     for tag in tags:
         if tag["class"] == "mvb":
             date = datetime.strptime(tag.b.string, "%A, %d %B %Y").date()
         else:
             # 'c1' indicates home team, 'c2' is hyphen-separated score, 'c3' is away team.
-            home_team = tag.tr.find("td", attrs={"class":"c1"}).a.string.extract()
+            home_team_cell = tag.tr.find("td", attrs={"class":"c1"}).b
+            home_team = home_team_cell.string.extract() if home_team_cell.a == None else home_team_cell.a.string.extract()
             score = tag.tr.find("td", attrs={"class":"c2"}).b.string.extract().split("-")
-            away_team = tag.tr.find("td", attrs={"class":"c3"}).a.string.extract()
+            away_team_cell = tag.tr.find("td", attrs={"class":"c3"}).b
+            away_team = away_team_cell.string.extract() if away_team_cell.a == None else away_team_cell.a.string.extract()
             results.append(Result(date, home_team, int(score[0]), away_team, int(score[1])))
     results.sort()
     return results
 
+def update_all(data_files):
+    for url, file in data_files.items():
+        new_results = scrape_bbc_results(url)
+        print "\n".join([str(result) for result in new_results])
+        print
 
-print "\n".join([str(result) for result in scrape_bbc_results("http://news.bbc.co.uk/sport1/hi/football/eng_prem/results/default.stm")])
+update_all(data)
