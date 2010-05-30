6044ac8e6c7e6c577ff05aff3e78d2713bcdff31
dwdyer <dan@uncommons.org>
2010-05-30 23:22:57 +0100

    Map goal-scorer names to canonical forms when scraping.

--- src/python/anorak/config.py
+++ src/python/anorak/config.py
@@ -17,7 +17,7 @@ def get_files_to_update(config_path):
 
 
 # Maps common team name variants to the canonical form of that team's name.
-def load_aliases(mappings_path):
+def load_team_aliases(mappings_path):
     """Load mappings for team names.  Maps abbreviated forms to canonical names."""
     print "Reading team name mapping file: %s" % mappings_path
     with open(mappings_path) as mappings_file:
@@ -26,5 +26,20 @@ def load_aliases(mappings_path):
         while line:
             values = line.split("=")
             mappings[values[0]] = values[1]
+            line = mappings_file.readline().rstrip()
+    return mappings
+
+
+# Maps short versions (i.e. just surname) and variations of a player's name to the canonical form.
+def load_player_aliases(mappings_path):
+    """Load mappings for player names.  Maps abbreviated forms to canonical names."""
+    print "Reading player name mapping file: %s" % mappings_path
+    with open(mappings_path) as mappings_file:
+        mappings = {}
+        line = mappings_file.readline().rstrip()
+        while line:
+            item = line.split("=")
+            key = item[0].split("|")
+            mappings[(key[0], key[1])] = item[1]
             line = mappings_file.readline().rstrip()
     return mappings
--- src/python/anorak/scraper.py
+++ src/python/anorak/scraper.py
@@ -3,7 +3,7 @@ import re
 import sys
 import urllib
 from BeautifulSoup import BeautifulSoup, SoupStrainer
-from config import get_files_to_update, load_aliases
+from config import get_files_to_update, load_player_aliases, load_team_aliases
 from results import Goal, Result, parse_rlt, write_rlt
 
 def load_html(url, strainer=None):
@@ -17,7 +17,7 @@ def load_html(url, strainer=None):
 # Regex to separate a goal scorer's name from the list of minutes in which they scored.
 scorer_regex = re.compile(r"(\D+?)\s\(([\w\s,\+]+)\)")
 
-def scrape_bbc_results(results_page_url, aliases):
+def scrape_bbc_results(results_page_url, team_aliases = {}, player_aliases = {}):
     """Scrapes the specified BBC web page for football results.  Returns a list of Result objects."""
     # Find all tags that contain dates or results ('mvb' indicates a date, 'competitionResults' indicates a result).
     tags = load_html(results_page_url, SoupStrainer(attrs={"class":["mvb", "competitionResults"]}))
@@ -27,9 +27,9 @@ def scrape_bbc_results(results_page_url, aliases):
             date = datetime.strptime(tag.b.string, "%A, %d %B %Y").date()
         else:
             # 'c1' indicates home team, 'c2' is hyphen-separated score, 'c3' is away team.
-            (home_team, home_goals) = parse_team(tag.tr.find("td", attrs={"class":"c1"}), aliases)
+            (home_team, home_goals) = parse_team(tag.tr.find("td", attrs={"class":"c1"}), team_aliases, player_aliases)
             score = tag.tr.find("td", attrs={"class":"c2"}).b.string.extract().split("-")
-            (away_team, away_goals) = parse_team(tag.tr.find("td", attrs={"class":"c3"}), aliases)
+            (away_team, away_goals) = parse_team(tag.tr.find("td", attrs={"class":"c3"}), team_aliases, player_aliases)
             home_score = int(score[0])
             away_score = int(score[1])
             assert home_score == len(home_goals), "Number of %s goals (%d) does not match home score (%d)." % (home_team, len(home_goals), home_score)
@@ -38,15 +38,21 @@ def scrape_bbc_results(results_page_url, aliases):
     return results
 
 
-def parse_team(tag, aliases = {}):
+def parse_team(tag, team_aliases = {}, player_aliases = {}):
     """Extracts a team name and list of goal-scorers from an HTML fragment. Returns a 2-item tuple."""
     team = tag.b.string.extract() if tag.b.a == None else tag.b.a.string.extract()
-    team = team if not team in aliases else aliases[team]
+    if team in team_aliases:
+        team = team_aliases[team]
     scorer_list = tag.find("p", attrs={"class":"scorer"}).findAll(text=True)
     goals = []
     for record in scorer_list:
         match = scorer_regex.match(record)
-        goals.extend(parse_goals(match.group(1), match.group(2)))
+        player = match.group(1)
+        if (team, player) in player_aliases:
+            player = player_aliases[(team, player)]
+        else:
+            print "Unknown %s player %s" % (team, player)
+        goals.extend(parse_goals(player, match.group(2)))
     goals.sort()
     return (team, goals)
 
@@ -64,10 +70,10 @@ def parse_goals(scorer, text):
     return goals
     
 
-def update_all(data_files, aliases):
+def update_all(data_files, team_aliases = {}, player_aliases = {}):
     for url, file in data_files.items():
         old_results, metadata = parse_rlt(file)
-        new_results = scrape_bbc_results(url, aliases)
+        new_results = scrape_bbc_results(url, team_aliases, player_aliases)
         combined_results = list(set(old_results) | set(new_results))
         if len(combined_results) > len(old_results):
             print "Writing %d new results to %s." % (len(combined_results) - len(old_results), file)
@@ -77,4 +83,4 @@ def update_all(data_files, aliases):
             print "No new results for %s, skipping." % file
 
 # Program entry point.  Expects two arguments, path to config file and path to mapping file.
-update_all(get_files_to_update(sys.argv[1]), load_aliases(sys.argv[2]))
+update_all(get_files_to_update(sys.argv[1]), load_team_aliases(sys.argv[2]), load_player_aliases(sys.argv[3]))
