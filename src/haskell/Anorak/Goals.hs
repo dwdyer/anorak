dd4b84b3c7e5cdb837eb0056cfdf5a579181b878
dwdyer <dan@uncommons.org>
2010-06-04 14:09:36 +0100

    Restrict number of players listed as top scorers to the ~25 highest scorers.

--- src/haskell/Anorak/Goals.hs
+++ src/haskell/Anorak/Goals.hs
@@ -1,14 +1,16 @@
 module Anorak.Goals (teamGoalScorers, topGoalScorers) where
 
 import Anorak.Results
+import Anorak.Utils(takeAtLeast)
 import Data.List(foldl', groupBy, nub, sortBy)
 import Data.Ord(comparing)
 
 -- | Generate a list of all goal scorers for a given set of results.
 topGoalScorers :: [Result] -> [(String, Int, [Team])]
-topGoalScorers results = sortBy (\(_, g1, _) (_, g2, _) -> compare g2 g1) $ map (\s -> (scorer.fst $ head s, length s, nub $ map snd s)) byScorer
+topGoalScorers results = takeAtLeast 25 $ groupBy (\(_, n, _) (_, m, _) -> n == m) scorers
                          where goals = concatMap extractGoals results
                                byScorer = groupByScorer (scorer.fst) $ filter ((/=) "o".goalType.fst) goals -- Omit own goals and group by scorer.
+                               scorers = sortBy (\(_, g1, _) (_, g2, _) -> compare g2 g1) $ map (\s -> (scorer.fst $ head s, length s, nub $ map snd s)) byScorer
 
 extractGoals :: Result -> [(Goal, Team)]
 extractGoals result = getGoals homeTeam homeGoals ++ getGoals awayTeam awayGoals
